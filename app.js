9b7797cf201a1955c558f624c9433bf294a3ab37
Michael Bravo <michael.bravo777@gmail.com>
2019-04-29 15:00:54 -0500

    added error check for name and age

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 function createGreeting(name, age) {
-  try {
+  if (typeof name === "string" && typeof age === "number") {
+    try {
     const yearOfBirth = getYearOfBirth(age);
     return `Hi, my name is ${name} and I'm ${age} years old.  I was born in ${yearOfBirth}.`;
    } catch (e) {
      console.log(e);
    }
+  } else {
+    throw new Error("Arguments not valid");
+  }
 };
 
 function getYearOfBirth(age) {
@@ -15,5 +19,5 @@ function getYearOfBirth(age) {
   return yearOfBirth;
 };
 
-const greeting1 = createGreeting('Chris', -29);
+const greeting1 = createGreeting("Chris", "twenty-nine");
 console.log(greeting1);
