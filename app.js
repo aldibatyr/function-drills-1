5aceffa2474a640abf76fd90e176d43aa4ce2ae3
Michael Bravo <michael.bravo777@gmail.com>
2019-04-29 14:52:30 -0500

    added try/catch

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 function createGreeting(name, age) {
-  const yearOfBirth = getYearOfBirth(age);
-  return `Hi, my name is ${name} and I'm ${age} years old.  I was born in ${yearOfBirth}.`;
+  try {
+    const yearOfBirth = getYearOfBirth(age);
+    return `Hi, my name is ${name} and I'm ${age} years old.  I was born in ${yearOfBirth}.`;
+   } catch (e) {
+     console.log(e);
+   }
 };
 
 function getYearOfBirth(age) {
   const yearOfBirth = 2019 - age; 
+  if (age < 0) {
+    throw new Error("Age can not be negative");
+  }
   return yearOfBirth;
 };
 
-const greeting1 = createGreeting('Chris', 29);
+const greeting1 = createGreeting('Chris', -29);
 console.log(greeting1);
