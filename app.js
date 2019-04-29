89f2560d0187e200faba025e5b28ab2fd435719f
Michael Bravo <michael.bravo777@gmail.com>
2019-04-29 14:45:05 -0500

    added function getYearOfBirth

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 function createGreeting(name, age) {
-  const yearOfBirth = 2019 - age; 
+  const yearOfBirth = getYearOfBirth(age);
   return `Hi, my name is ${name} and I'm ${age} years old.  I was born in ${yearOfBirth}.`;
+};
+
+function getYearOfBirth(age) {
+  const yearOfBirth = 2019 - age; 
+  return yearOfBirth;
 };
 
 const greeting1 = createGreeting('Chris', 29);
